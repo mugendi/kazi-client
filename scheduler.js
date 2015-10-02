4ddf1f08b1f6eb454ddd6baaeb076b53c7be9784
Anthony Mugendi <ngurumugz@gmail.com>
2015-10-02 07:25:31 +0300

    Changes to client reloads

--- scheduler.js
+++ scheduler.js
@@ -7,36 +7,39 @@ var util = require('util');
 var jobs=[]
 
 //add jobs
-jobs.push(
-	{		
-		id:'Tracking:Kenya',
-		name:'twitter-tracker',		
-		data:{
-			query:'Kenya OR Kenia'
-		},
-		meta:{
-			ttl: (1*1000*60), //this job, irrespective of its state is killed after 10 minutes. Killing is graceful and only happens when a job is not runnibg (idle) or has completed
-			delay:20, //how long should we wait before running job
-			priority:'normal', //whats the job priority (defaults to 'low', 'normal' & 'high' )
-			reschedule_after:(10*1000*60) //this value forces a job to complete after 90 seconds. This is different from ttl which 'KILLS' the job. With completion, it is assumed that the worker hang or crushed and it is important to keep the job for another worker. KILL on the other hand means the job is never run by any other worker. 'rescheduleAfter' is actually a safeguard to ensure that if a worker hasn't returned a job status for too ling, then the server can automatically reschedule the job and allocate to any other free workers. Because a job can only be served to one client at a time, without this value, hang-up jobs would never get executed!
-		}
-	}
-);
+// jobs.push(
+// 	{		
+// 		id:'Tracking:Kenya',
+// 		name:'twitter-tracker',		
+// 		data:{
+// 			query:'Kenya OR Kenia'
+// 		},
+// 		meta:{
+// 			ttl: (1*1000*60), //this job, irrespective of its state is killed after 10 minutes. Killing is graceful and only happens when a job is not runnibg (idle) or has completed
+// 			delay:20, //how long should we wait before running job
+// 			priority:'normal', //whats the job priority (defaults to 'low', 'normal' & 'high' )
+// 			reschedule_after:(10*1000*60) //this value forces a job to complete after 90 seconds. This is different from ttl which 'KILLS' the job. With completion, it is assumed that the worker hang or crushed and it is important to keep the job for another worker. KILL on the other hand means the job is never run by any other worker. 'rescheduleAfter' is actually a safeguard to ensure that if a worker hasn't returned a job status for too ling, then the server can automatically reschedule the job and allocate to any other free workers. Because a job can only be served to one client at a time, without this value, hang-up jobs would never get executed!
+// 		}
+// 	}
+// );
 
 
 //NOTE: all values passed in the meta attribute above can also be passed within the main job object
 jobs.push(
 	{
 		priority:'high',
-		id:'Tracking:US',
-		name:'twitter-tracker',
+		id:'Tracking:Museveni',
+		name:'tracking.twitter.track',
 		data:{
-			query:'US OR America OR USA'
+			job_id:'Tracking:Museveni',
+			locale: 'ke',
+			terms : ['museveni'],
+			platforms: ['twitter'],
 		},
 		ttl: 0, // (3*60*1000*60), //3hours
 		reschedule_after: (20*1000*60), //20 mins
 		reschedule_after: (10*1000*60), //10 mins
-		delay:10 //execute immediately
+		delay:0 //execute immediately
 
 	}
 );
